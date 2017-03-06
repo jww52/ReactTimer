84cb8a0d0315f4170f9e719488b9df5f6ea59fad
jww52 <jawszone@gmail.com>
2017-03-05 22:39:54 -0600

    Added navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Countdown = (props) => {
+    return (
+      <div>
+        <p>Countdown Component</p>
+      </div>
+    );
+};
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Timer = (props) => {
+    return (
+      <div>
+        <p>Timer Component</p>
+      </div>
+    );
+};
+
+module.exports = Timer;
